3a8c628b64f06c8e6e385be00eec0aa7bb508447
SpeedoMacMuffin <klarmann1@googlemail.com>
2021-05-06 11:51:00 +0200

    added array_range, remove later

--- public/js/socket.js
+++ public/js/socket.js
@@ -1,2 +1,10 @@
 // SOCKET IO
 const socket = io();
+
+const array_range = (start, len) => {
+    const arr = new Array(len);
+    for (let i = 0; i < len; i++, start++) {
+        arr[i] = start;
+    }
+    return arr;
+};
