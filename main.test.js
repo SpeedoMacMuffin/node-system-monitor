4cb19cb5a26bcea6818d590a626e4e9f46875f4b
SpeedoMacMuffin <klarmann1@googlemail.com>
2021-05-07 13:03:35 +0200

    added test for si.currentLoad

--- main.test.js
+++ main.test.js
@@ -1,30 +1,13 @@
-const Chart = require('chart.js')
+const si = require('systeminformation');
 
-it('renders correctly', () => {
-    const el = document.createElement('canvas')
-    let myChart = new Chart(el, {
-        type: 'line',
-        data: {
-            labels: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-            datasets: [{
-                label: 'CPU Usage in %',
-                data: [100, 50, 75, 25, 33, 66, 20, 40, 60, 80],
-            }]
-        },
-        options: {
-            responsive: true,
-            animation: false,
-            responsive: true,
-            scales: {
-                yAxes: [{
-                    ticks: {
-                        min: 0,
-                        max: 100,
-                    }
-                }]
-
-            }
+test('Cpu Info does not return null', () => {
+    const cpuUsage = async() => {
+        try {
+            const cpu = await si.currentLoad();
+            expect(cpu).toNotBe(null);
+            done();
+        } catch (e) {
+            done(e);
         }
-    })
-    expect(myChart).toMatchSnapshot();
+    };
 });
