e407e5eb57bc949fc55701312af1a6d005342ae5
SpeedoMacMuffin <klarmann1@googlemail.com>
2021-05-07 13:02:23 +0200

    removed os-package, moved functions for static values to /src/systemInfo.js

--- main.js
+++ main.js
@@ -2,86 +2,58 @@ const http = require('http');
 const express = require('express');
 const app = express();
 const httpServer = http.createServer(app);
-const osUtils = require('node-os-utils');
-const os = require('os');
 const io = require('socket.io')(httpServer);
 const si = require('systeminformation');
+const osUtils = require('node-os-utils');
+const systemInfo = require('./src/systemInfo');
 
 // View Engine
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 
-// initiating variables for static information
-
-// username
-const username = os.userInfo([{ encoding: 'buffer' }]).username;
-
-// drive information
-let drive = osUtils.drive.info().then(data => {
-    drive = data;
-});
-
-// cpu information
-let cpu = si
-    .cpu()
-    .then(data => {
-        cpu = data;
-    })
-    .catch(error => console.error(error));
-
-// ram information
-let ram = si
-    .mem()
-    .then(data => {
-        ram = (data.total / Math.pow(1024, 3)).toFixed(2); //converts initial value from Bytes to GigaBytes
-    })
-    .catch(error => console.error(error));
-
-// operating system information
-let osInfo = si
-    .osInfo()
-    .then(data => {
-        osInfo = data;
-    })
-    .catch(error => console.error(error));
-
-// system information
-let sysInfo = si
-    .system()
-    .then(data => {
-        sysInfo = data;
-    })
-    .catch(error => console.error(error));
-
 // Route
-app.get('/', (__, res) => {
+app.get('/', async(__, res) => {
     res.render('index.ejs', {
-        username,
-        drive,
-        cpu,
-        ram,
-        osInfo,
-        sysInfo,
+        username: await systemInfo.getUser(),
+        drives: await systemInfo.getDrives(),
+        cpu: await systemInfo.getCpu(),
+        ram: await systemInfo.getRam(),
+        osInfo: await systemInfo.getOsInfo(),
+        sysInfo: await systemInfo.getSysInfo(),
     });
 });
 
 // SOCKET IO for dynamic value updates (cpu- & ram-usage)
-io.on('connection', socket => {
+io.on('connection', async socket => {
     console.log(`${socket.id} connected`);
     // Refresh monitor after 1s - send updated stats
     setInterval(() => {
-        // RAM used (total - free)
-        let ramUsed = Math.round(os.totalmem()) - Math.round(os.freemem());
         // RAM usage in %
-        osUtils.mem
-            .info()
-            .then(info => socket.emit('ramUsage', info))
-            .catch(error => console.error(error));
+        const ramUsage = async() => {
+            try {
+                const ram = await si.mem();
+                socket.emit('ramUsage', {
+                    used: systemInfo.byteToGigabyte(ram.used),
+                    total: systemInfo.byteToGigabyte(ram.total),
+                });
+            } catch (e) {
+                console.error(e);
+            }
+        };
+        ramUsage();
         // CPU usage in %
-        osUtils.cpu
-            .usage()
-            .then(cpu => socket.emit('cpuUsage', cpu))
-            .catch(error => console.error(error));
+        const cpuUsage = async() => {
+            try {
+                const cpu = await si.currentLoad();
+                socket.emit('cpuUsage', cpu.currentLoad);
+            } catch (e) {
+                console.error(e);
+            }
+        };
+        //cpuUsage(); // module returns random 'null'-values, added node-os-utils cpu.usage instead to run stable
+        osUtils.cpu.usage().then(cpu => {
+            socket.emit('cpuUsage', cpu);
+        });
     }, 1000);
 });
 
